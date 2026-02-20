68ab12e2b9909b7a4e8ce2c6309871b2d250bc01
favas-cv <fawazmuhd0@gmail.com>
2026-02-20 11:06:48 +0530

    updated production setting

--- peakpackbackend/settings.py
+++ peakpackbackend/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from decouple import config 
 from pathlib import Path
 
+ 
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,8 +27,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',cast=bool)
 
 
 ALLOWED_HOSTS = config(
@@ -33,6 +34,12 @@ ALLOWED_HOSTS = config(
     cast=lambda v: [s.strip() for s in v.split(",")]
 )
 
+
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SESSION_COOKIE_SECURE = not DEBUG
+CSRF_COOKIE_SECURE = not DEBUG
+SECURE_SSL_REDIRECT = False
 
 # Application definition
 

9219b92beec1f4f2f1020e8de7c8a92e25bdd730
favas-cv <fawazmuhd0@gmail.com>
2026-02-21 08:03:46 +0530

    Fix URL patterns by removing leading slashes

--- paymentsetup/urls.py
+++ paymentsetup/urls.py
@@ -3,6 +3,6 @@ from .views import CreateRazorpayOrder,VerifyPayment
 from django.urls import path
 
 urlpatterns = [
-    path('/create-razorpay-order/',CreateRazorpayOrder.as_view()),
-    path('/verify-payment/',VerifyPayment.as_view()),
+    path('create-razorpay-order/',CreateRazorpayOrder.as_view()),
+    path('verify-payment/',VerifyPayment.as_view()),
 ]
